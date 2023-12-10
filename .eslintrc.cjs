452866228833b780409771fbf79a2f6fb89a27a4
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-10 15:22:37 +0200

    first test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'cypress/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -10,11 +10,20 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'cypress'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
+    'cypress/no-async-tests': 'error',
+    'cypress/no-pause': 'error'
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import './App.css'
 
 function App() {
   return (
-    <div>Empty project</div>
+    <div>
+      <h2>Test list elements</h2>
+      <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+        <li>List 4</li>
+        <li>List 5</li>
+        <li>List 6</li>
+      </ul>
+    </div>
   )
 }
 
