2cf71577f22d0e1fea9929ae34ab59b034de1481
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-10 19:08:16 +0200

    project skeleton for testing training

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
-import './App.css'
+import { useState } from 'react';
+import Header from './components/Header';
+import Modal from './components/Modal';
+
+import NewTask from './components/NewTask';
+import TaskControl from './components/TaskControl';
+import TaskList from './components/TaskList';
 
 function App() {
+  const [isAddingTask, setIsAddingTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [appliedFilter, setAppliedFilter] = useState('all');
+
+  const displayedTasks = tasks.filter((task) => {
+    if (appliedFilter === 'all') {
+      return true;
+    }
+    return task.category === appliedFilter;
+  });
+
+  function startAddTaskHandler() {
+    setIsAddingTask(true);
+  }
+
+  function cancelAddTaskHandler() {
+    setIsAddingTask(false);
+  }
+
+  function addTaskHandler(taskData) {
+    setTasks((prevTasks) => {
+      return [
+        ...prevTasks,
+        {
+          id: Math.random().toString(),
+          ...taskData,
+        },
+      ];
+    });
+    setIsAddingTask(false);
+  }
+
+  function setFilterHandler(category) {
+    setAppliedFilter(category);
+  }
+
   return (
-    <div>
-      <h2>Test list elements</h2>
-      <ul>
-        <li>List 1</li>
-        <li>List 2</li>
-        <li>List 3</li>
-        <li>List 4</li>
-        <li>List 5</li>
-        <li>List 6</li>
-      </ul>
-    </div>
-  )
+    <>
+      {isAddingTask && (
+        <Modal onClose={cancelAddTaskHandler}>
+          <NewTask onAddTask={addTaskHandler} onCancel={cancelAddTaskHandler} />
+        </Modal>
+      )}
+      <Header />
+      <main>
+        <TaskControl
+          onStartAddTask={startAddTaskHandler}
+          onSetFilter={setFilterHandler}
+        />
+        <TaskList tasks={displayedTasks} />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
