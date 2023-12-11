9ddca0c6c91edc5026c6d4ac571382f513a88b67
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-11 15:05:28 +0200

    typing into input fields and creation of new task

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -8,5 +8,24 @@ describe('tasks management', () => {
         cy.get('.backdrop').click({ force: true });
         cy.get('.backdrop').should('not.exist');
         cy.get('.modal').should('not.exist');
+
+        cy.contains('Add Task').click();
+        cy.get('.actions button').contains('Cancel').click();
+        cy.get('.modal').should('not.exist');
+        cy.get('.backdrop').should('not.exist');
+    });
+
+    it('should create a new task', () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('#task-control button')
+            .contains('Add Task').click();
+        cy.get('#title').type('New task');
+        cy.get('#summary').type('some summary');
+        cy.get('.actions button').contains('Add Task').click();
+        cy.get('.modal').should('not.exist');
+        cy.get('.backdrop').should('not.exist');
+        cy.get('.task').should('have.length', 1);
+        cy.get('.task h2').contains('New task');
+        cy.get('.task p').contains('some summary');
     });
 });
