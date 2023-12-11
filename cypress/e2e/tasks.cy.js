25ca90f2a2b85a140a5dc3c901414d19757cf204
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-11 20:01:21 +0200

    filtering and adding multiple tasks

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -38,4 +38,43 @@ describe('tasks management', () => {
         cy.get('.actions button').contains('Add Task').click();
         cy.get('#new-task-form p.error-message').contains('Please provide values for');
     });
+
+    it('should filter tasks', () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('#task-control button')
+            .contains('Add Task').click();
+        cy.get('#title').type('New task');
+        cy.get('#summary').type('some summary');
+        cy.get('#category').select('urgent');
+        cy.get('.actions button').contains('Add Task').click();
+        cy.get('.task').should('have.length', 1);
+        cy.get('#filter').select('low');
+        cy.get('.task').should('have.length', 0);
+        cy.get('#filter').select('urgent');
+        cy.get('.task').should('have.length', 1);
+        cy.get('#filter').select('all');
+        cy.get('.task').should('have.length', 1);
+    });
+
+    it('should add multiple tasks', () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('#task-control button')
+            .contains('Add Task').click();
+        cy.get('#title').type('Task 1');
+        cy.get('#summary').type('first task');
+        cy.get('#category').select('low');
+        cy.get('.actions button').contains('Add Task').click();
+        cy.get('.task').should('have.length', 1);
+
+        cy.get('#task-control button')
+            .contains('Add Task').click();
+        cy.get('#title').type('Task 2');
+        cy.get('#summary').type('second task');
+        cy.get('#category').select('low');
+        cy.get('.actions button').contains('Add Task').click();
+        cy.get('.task').should('have.length', 2);
+
+        cy.get('.task').eq(0).contains('Task 1');
+        cy.get('.task').eq(1).contains('Task 2');
+    });
 });
