c7a05165eedecc9ffad5d362bb21913dafa32491
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-16 23:47:59 +0200

    added a second stub for the clipboard method

--- section-5/cypress/e2e/location.cy.js
+++ section-5/cypress/e2e/location.cy.js
@@ -1,11 +1,37 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then(window => {
-      cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+      cy.stub(window.navigator.geolocation, 'getCurrentPosition')
+        .as('getUserPosition')
+        .callsFake((callback) => {
+          setTimeout(() => {
+            callback({ coords: {
+                latitude: 37.5,
+                longitude: 48.01
+              },
+            });
+          }, 100);
+        });
+
+        cy.stub(window.navigator.clipboard, 'writeText')
+          .as('saveToClipboard')
+          .resolves();
     });
+  });
+
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain', 'Location fetched'); // alternative to contains
+  });
+
+  it('should share a location URL', () => {
+    cy.get('[data-cy="name-input"]').type('Jonh Doe');
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('@saveToClipboard').should('have.been.called');
   });
 });
