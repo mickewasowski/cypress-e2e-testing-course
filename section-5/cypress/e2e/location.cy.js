bc9950f75ea324c7298e7c6e7d9bdf69aa705c2d
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-16 18:14:55 +0200

    creating & using stubs

--- section-5/cypress/e2e/location.cy.js
+++ section-5/cypress/e2e/location.cy.js
@@ -2,7 +2,10 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then(window => {
+      cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called');
   });
 });
