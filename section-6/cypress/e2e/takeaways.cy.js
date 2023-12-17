bc0c64283b9ba811752561330e22578b6939b61d
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-17 16:22:54 +0200

    seeding data before each test

--- section-6/cypress/e2e/takeaways.cy.js
+++ section-6/cypress/e2e/takeaways.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress" />
 
 describe('Takeaways', () => {
+  beforeEach(() => {
+    cy.task('seedDatabase');
+  });
+
   it('should display a list of fetched takeaways', () => {
     cy.visit('/')
+    cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
   });
 });
