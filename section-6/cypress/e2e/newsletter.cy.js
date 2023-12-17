f4811562c204596428de3b9d9163a2cc951bd1bc
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-17 16:50:00 +0200

    using intercepter

--- section-6/cypress/e2e/newsletter.cy.js
+++ section-6/cypress/e2e/newsletter.cy.js
@@ -7,9 +7,11 @@ describe('Newsletter', () => {
     });
 
     it('should display a success message', () => {
+        cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe'); // intercept any HTTP request send to localhost:3030/newletter
         cy.visit('/');
         cy.get('[data-cy="newsletter-email"]').type('test@example.com');
         cy.get('[data-cy="newsletter-submit"]').click();
+        cy.wait('@subscribe');
         cy.contains('Thanks for signing up');
     });
 });
