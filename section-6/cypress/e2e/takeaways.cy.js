bff328a8d31bad7dde460c4917340c21eaf98425
Rosen Yorgov <yorgov.h.r@gmail.com>
2023-12-17 17:35:39 +0200

    create new takeaway and intercept the request

--- section-6/cypress/e2e/takeaways.cy.js
+++ section-6/cypress/e2e/takeaways.cy.js
@@ -9,4 +9,15 @@ describe('Takeaways', () => {
     cy.visit('/')
     cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
   });
+
+  it('should add new takeaway', () => {
+    cy.intercept('POST', '/takeaways/new*', { status: 201 }).as('createTakeaway');
+    cy.login();
+    cy.visit('/takeaways/new');
+    cy.get('[data-cy="title"]').click();
+    cy.get('[data-cy="title"]').type('Testtitle1');
+    cy.get('[data-cy="body"]').type('TestBody1');
+    cy.get('[data-cy="create-takeaway"]').click();
+    cy.wait('@createTakeaway').its('request.body').should('match', /Testtitle1.*TestBody1/);
+  });
 });
